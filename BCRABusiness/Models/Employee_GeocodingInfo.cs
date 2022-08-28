0bcd090656aad9e9599b41cb8fcbf27263de29fc
LoFaroBruno <110917929+LoFaroBruno@users.noreply.github.com>
2022-08-28 14:52:42 -0300

    New Employee derived class with geocoding info

---
+++ BCRABusiness/Models/Employee_GeocodingInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BCRABusiness.Models
+{
+    internal class Employee_GeocodingInfo : Employee
+    {
+        [Required(ErrorMessage = "City is Required")]
+        [StringLength(50)]
+        public string City { get; set; }
+    }
+}
--- WebAPI/Controllers/EmployeesController.cs
+++ WebAPI/Controllers/EmployeesController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Web.Http;
 using WebAPI.Filters;
-using System.ComponentModel;
 using DataAccess;
 using BCRABusiness.Models;
 
